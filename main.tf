0328300e51ed620a8e623d48a8e72f4b7fc52604
Tobias Rupprecht <trupprecht@hashicorp.com>
2024-11-19 11:21:00 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+# Provider configuration
+provider "aws" {
+  region = var.region
+}
+
+module "aws_config" {
+  source = "trussworks/config/aws"
+
+  config_name        = "my-aws-config"
+  config_logs_bucket = "my-aws-logs"
+}
